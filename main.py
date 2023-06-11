57b05ec076d79f7998d1c1d2f512e8e7c2800758
Marnus2 <marnusl365@gmail.com>
2023-06-11 16:28:53 +0200

    Added images

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Marnus Lourens")
+    content = """
+    """
+    st.write(content)
