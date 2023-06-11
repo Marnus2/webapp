fab50844c6692305d0fcbd35cd194384a50c4b7b
Marnus2 <marnusl365@gmail.com>
2023-06-11 16:32:42 +0200

    Created web page

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 col1, col2 = st.columns(2)
 
 with col1:
@@ -9,4 +11,4 @@ with col2:
     st.title("Marnus Lourens")
     content = """
     """
-    st.write(content)
+    st.info(content)
