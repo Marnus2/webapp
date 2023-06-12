d75d1ac47cad941eeb5f3360b92ddd8a8c6a6e59
Marnus2 <marnusl365@gmail.com>
2023-06-12 17:31:58 +0200

    Day 22: Render app titles into columns

--- main.py
+++ main.py
@@ -26,7 +26,14 @@ df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
